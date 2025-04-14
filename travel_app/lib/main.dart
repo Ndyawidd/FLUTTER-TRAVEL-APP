c6301feb3b850f4569a38b9603124cb8fc1fca70
ndyawidd <widdynadya214@gmail.com>
2025-04-14 22:12:00 +0700

    fix revert

--- travel_app/lib/main.dart
+++ travel_app/lib/main.dart
@@ -3,7 +3,7 @@ import 'routes/app_routes.dart';
 import 'presentation/pages/splash/splash.dart';
 import 'presentation/pages/auth/login_page.dart';
 import 'presentation/pages/auth/register_page.dart';
-// import 'presentation/pages/user/home/home.dart';
+import 'presentation/pages/user/home/home.dart';
 // import 'presentation/pages/booking/booking.dart';
 // import 'presentation/pages/wishlist/wishlist.dart';
 // import 'presentation/pages/feedback/feedback.dart';
@@ -11,7 +11,6 @@ import 'presentation/pages/admin/order/order.dart';
 // import 'presentation/pages/admin/review/review.dart';
 import 'presentation/pages/admin/ticket/ticket.dart';
 // import 'presentation/pages/admin/ticket/ticketdetail.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -32,18 +31,16 @@ class MyApp extends StatelessWidget {
         AppRoutes.login: (_) => const LoginPage(),
         AppRoutes.register: (_) => const RegisterPage(),
 
-
         // User pages
         '/user/home': (context) => const HomePage(),
         // '/destination/detail': (context) => DestinationDetailPage(
         // name: '', location: '', imageUrl: '', price: 0, // Dummy untuk compiler
 
-        AppRoutes.homeUser: (context) => const HomePage(),
+        // AppRoutes.homeUser: (context) => const HomePage(),
         // AppRoutes.booking: (context) => const BookingPage(),
         // AppRoutes.wishlist: (context) => const WishlistPage(),
         // AppRoutes.feedback: (context) => const FeedbackPage(),
         // AppRoutes.profile: (context) => const ProfilePage(),
-
 
         // Admin Pages
         AppRoutes.adminTicket: (_) => const TicketPage(),
